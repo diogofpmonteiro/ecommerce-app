2c14148bd80e99749aa7816de5e983967711d9d6
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2025-08-07 10:29:22 +0100

    customize styles to match neobrutalism inspo

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -9,11 +9,16 @@ function Progress({ className, value, ...props }: React.ComponentProps<typeof Pr
   return (
     <ProgressPrimitive.Root
       data-slot='progress'
-      className={cn("bg-primary/20 relative h-2 w-full overflow-hidden rounded-full", className)}
+      className={cn(
+        "relative h-3 w-full overflow-hidden rounded-full",
+        // Modified classes
+        "border bg-transparent",
+        className
+      )}
       {...props}>
       <ProgressPrimitive.Indicator
         data-slot='progress-indicator'
-        className='bg-primary h-full w-full flex-1 transition-all'
+        className='bg-pink-400 h-full w-full flex-1 transition-all'
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
     </ProgressPrimitive.Root>
