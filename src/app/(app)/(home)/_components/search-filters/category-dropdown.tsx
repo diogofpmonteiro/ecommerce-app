0af70ff04a4422316bc6d86535b123c7b3ee2d46
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2025-08-10 19:23:58 +0100

    update category schema, add subcategories, create basic UI for category dropdown

---
+++ src/app/(app)/(home)/_components/search-filters/category-dropdown.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Category } from "@/payload-types";
+
+interface CategoryDropdownProps {
+  category: Category;
+  isActive: boolean;
+  isNavigationHovered: boolean;
+}
+
+export const CategoryDropdown = ({ category, isActive, isNavigationHovered }: CategoryDropdownProps) => {
+  return (
+    <Button
+      variant='elevated'
+      className={cn(
+        "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
+        isActive && !isNavigationHovered && "bg-white border-primary"
+      )}>
+      {category.name}
+    </Button>
+  );
+};
--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,14 +1,39 @@
+import { getPayload } from "payload";
 import { Footer } from "./_components/footer";
 import { Navbar } from "./_components/navbar";
+import { SearchFilters } from "./_components/search-filters";
+import configPromise from "@payload-config";
+import { Category } from "@/payload-types";
 
 interface HomeLayoutProps {
   children: React.ReactNode;
 }
 
-export default function HomeLayout({ children }: HomeLayoutProps) {
+export default async function HomeLayout({ children }: HomeLayoutProps) {
+  const payload = await getPayload({ config: configPromise });
+
+  const data = await payload.find({
+    collection: "categories",
+    depth: 1, // populate subcategories
+    where: {
+      parent: {
+        exists: false, // fetch top-level categories only
+      },
+    },
+  });
+
+  const formattedData = data.docs.map((category) => ({
+    ...category,
+    subcategories: (category.subcategories?.docs ?? []).map((subcategory) => ({
+      ...(subcategory as Category), // we can infer the type here because of depth: 1
+      subcategories: undefined,
+    })),
+  }));
+
   return (
     <div className='flex flex-col min-h-screen'>
       <Navbar />
+      <SearchFilters data={formattedData} />
       <div className='flex-1 bg-[#f4f4f0]'>{children}</div>
       <Footer />
     </div>
