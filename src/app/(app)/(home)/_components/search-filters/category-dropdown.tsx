43a81d820ab60e098940f6057671a36cf02f5fd3
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2025-08-10 20:46:30 +0100

    finish dropdown menu position, add use-dropdown-position hook

--- src/app/(app)/(home)/_components/search-filters/category-dropdown.tsx
+++ src/app/(app)/(home)/_components/search-filters/category-dropdown.tsx
@@ -1,6 +1,10 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { useDropdownPosition } from "@/hooks/use-dropdown-position";
 import { cn } from "@/lib/utils";
 import { Category } from "@/payload-types";
+import { useRef, useState } from "react";
+import { SubcategoryMenu } from "./subcategory-menu";
 
 interface CategoryDropdownProps {
   category: Category;
@@ -9,14 +13,41 @@ interface CategoryDropdownProps {
 }
 
 export const CategoryDropdown = ({ category, isActive, isNavigationHovered }: CategoryDropdownProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const { getDropdownPosition } = useDropdownPosition(dropdownRef);
+
+  const onMouseEnter = () => {
+    if (category.subcategories) {
+      setIsOpen(true);
+    }
+  };
+
+  const onMouseLeave = () => setIsOpen(false);
+
   return (
-    <Button
-      variant='elevated'
-      className={cn(
-        "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
-        isActive && !isNavigationHovered && "bg-white border-primary"
-      )}>
-      {category.name}
-    </Button>
+    <div className='relative' ref={dropdownRef} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+      <div className='relative'>
+        <Button
+          variant='elevated'
+          className={cn(
+            "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
+            isActive && !isNavigationHovered && "bg-white border-primary"
+          )}>
+          {category.name}
+        </Button>
+        {category.subcategories && category.subcategories.length > 0 && (
+          <div
+            className={cn(
+              "opacity-0 absolute -bottom-3 w-0 h-0 border-l-[10px] border-r-[10px] border-b-[10px] border-l-transparent border-r-transparent border-b-black left-1/2 -translate-x-1/2",
+              isOpen && "opacity-100"
+            )}
+          />
+        )}
+      </div>
+
+      <SubcategoryMenu category={category} isOpen={isOpen} position={getDropdownPosition()} />
+    </div>
   );
 };
