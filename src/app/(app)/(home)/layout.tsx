f5a83e7fed8223491941831e657f87aa48dc37c9
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2025-08-18 16:55:14 +0100

    add modules/components folder

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,6 +1,6 @@
-import { Footer } from "./_components/footer";
-import { Navbar } from "./_components/navbar";
-import { SearchFilters, SearchFiltersSkeleton } from "./_components/search-filters";
+import { Footer } from "@/modules/home/ui/components/footer";
+import { Navbar } from "@/modules/home/ui/components/navbar";
+import { SearchFilters, SearchFiltersSkeleton } from "@/modules/home/ui/components/search-filters";
 import { HydrateClient, trpc } from "@/trpc/server";
 import { ErrorBoundary } from "react-error-boundary";
 import { Suspense } from "react";
--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -5,7 +5,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import { loginSchema } from "../../schemas";
+import { loginSchema } from "@/modules/auth/schemas";
 import z from "zod";
 import Link from "next/link";
 import { Poppins } from "next/font/google";
