98588cf4f04adc29de2dfa726623d3f8f04bea54
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2025-08-29 09:55:34 +0100

    implement checkout page, layout and procedures

--- src/app/(app)/(tenants)/tenants/[slug]/(checkout)/checkout/page.tsx
+++ src/app/(app)/(tenants)/tenants/[slug]/(checkout)/checkout/page.tsx
@@ -1,5 +1,13 @@
-const CheckoutPage = () => {
-  return <div>CheckoutPage</div>;
+import { CheckoutView } from "@/modules/checkout/ui/views/checkout-view";
+
+interface Props {
+  params: Promise<{ slug: string }>;
+}
+
+const CheckoutPage = async ({ params }: Props) => {
+  const { slug } = await params;
+
+  return <CheckoutView tenantSlug={slug} />;
 };
 
 export default CheckoutPage;
---
+++ src/modules/checkout/server/procedures.ts
@@ -0,0 +1,40 @@
+import z from "zod";
+
+import { TRPCError } from "@trpc/server";
+
+import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { Media, Tenant } from "@/payload-types";
+
+export const checkoutRouter = createTRPCRouter({
+  getProducts: baseProcedure
+    .input(
+      z.object({
+        ids: z.array(z.string()),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const data = await ctx.payload.find({
+        collection: "products",
+        depth: 2, // populates "image", "category", tenant & tenant.image
+        where: {
+          id: {
+            in: input.ids,
+          },
+        },
+      });
+
+      if (data.totalDocs !== input.ids.length) {
+        throw new TRPCError({ code: "NOT_FOUND", message: "Products not found" });
+      }
+
+      return {
+        ...data,
+        totalPrice: data.docs.reduce((acc, product) => acc + product.price, 0),
+        docs: data.docs.map((doc) => ({
+          ...doc,
+          image: doc.image as Media | null,
+          tenant: doc.tenant as Tenant & { image: Media | null },
+        })),
+      };
+    }),
+});
